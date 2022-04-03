102d3b789729fef92b8ca933ee5e4d8ae09dc261
GyeungHoon <ghp0415@naver.com>
2022-04-04 04:20:44 +0900

    Build project automatically 설정 켜고 devtools 설치, hot reload

--- src/main/java/com/ghp/myproject/user/controller/Usercontroller.java
+++ src/main/java/com/ghp/myproject/user/controller/Usercontroller.java
@@ -2,14 +2,16 @@ package com.ghp.myproject.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api/users")
 public class Usercontroller {
 
     @RequestMapping("")
+    @ResponseBody
     public String users(){
 
-        return "users";
+        return "us22ers";
     }
 }
